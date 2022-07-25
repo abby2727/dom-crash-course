65bc1df8541d8ac7787e094cb0ca95cc720a34ce
abby2727 <abdulfahadgo566@gmail.com>
2022-07-25 11:38:06 +0800

    Update Project

---
+++ dom.js
@@ -0,0 +1,49 @@
+// EXAMINE DOCUMENT OBJECT
+// console.dir(document);
+// console.log(document.domain);
+// console.log(document.location);
+// console.log(document.URL);
+// console.log('Title:' + document.title);
+// console.log(document.all);
+// console.log(document.links);
+
+// GET ELEMENT BY ID
+// var headerTitle = document.getElementById('header-title');
+// var header = document.getElementById('main-header');
+// headerTitle.textContent = 'Hello';
+// header.style.border = '3px solid #000';
+// headerTitle.style.color = 'yellow';
+
+// var fourthItem = document.getElementById('item-4');
+// // fourthItem.style.color = 'green';
+// fourthItem.style.fontWeight = 'bold';
+
+// GET ELEMENTS BY CLASS NAME
+// var items = document.getElementsByClassName('list-group-item');
+// console.log(items);
+// items[0].textContent = 'First Item';
+// items[3].textContent = 'Fourth Item';
+// items[0].style.fontWeight = 'bold';
+// // items[3].style.backgroundColor = 'lightgray';
+
+// for (var i = 0; i < items.length; i++) {
+//     items[i].style.backgroundColor = 'lightgray';
+// }
+
+// GET ELEMENTS BY TAGNAME
+
+// QUERY SELECTOR
+// var item = document.querySelector('.list-group-item');
+// item.style.color = 'red';
+// var lastItem = document.querySelector('.list-group-item:last-child');
+// lastItem.style.color = 'blue';
+// var thirItem = document.querySelector('.list-group-item:nth-child(3)');
+// thirItem.style.color = 'yellow';
+
+// QUERY SELECTOR ALL
+// var odd = document.querySelectorAll('li:nth-child(odd)');
+// var even = document.querySelectorAll('li:nth-child(even)');
+
+// for (var i = 0; i < even.length; i++) {
+//     even[i].style.backgroundColor = 'lightgray';
+// }
